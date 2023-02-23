f55dc1793c222ce49b85e2ad6e9303b59055a71c
SamuelMilla <samuelmillafs@outlook.com>
2023-02-23 20:04:50 -0300

    songs and img js

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ let track_index = 0;
 let isPlaying = false;
 let isRandom = false;
 let updateTimer;
+
+const music_list = [
+    {
+        img : 'images/Even-so.jpg',
+        name : 'Even so',
+        artist : 'Dosii',
+        music : 'songs/Dosii - Even so.mp3'
+    },
+
+    {
+        img: 'images/Anjo-de-papel.jpg',
+        name: '',
+        artist: '',
+        music: 'songs/Felicia rock - Anjo de papel.mp3'
+    },
+
+    {
+        img: 'images/Silent-Solitude.jpg',
+        name: '',
+        artist: '',
+        music: 'songs/Miura Jam Cover -Silent Solitude (PT BR).mp3'
+    },
+
+    {
+        img: 'images/Primary.jpg',
+        name: '',
+        artist: '',
+        music: 'songs/Primary - When I fall in love.mp3'
+    },
+
+    {
+        img: 'images/Starstarstar.jpg',
+        name: 'Starstarstar',
+        artist: 'Dosii',
+        music: 'songs/Dosii - Starstarstar.mp3'
+    },
+
+    {
+        img: 'images/Adesso-e-fortuna.jpg',
+        name: '',
+        artist: '',
+        music: 'songs/新居 昭乃 - Adesso e fortuna.mp3'
+    }
+];
